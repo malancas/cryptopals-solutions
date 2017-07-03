f2fb7edab8869e8626706dca817035bec48d103d
malancas <malancaster0@gmail.com>
2017-07-03 18:10:30 -0400

    Added code for challenge 1.4 in main. Added more functionsin c14

--- c14.scala
+++ c14.scala
@@ -11,10 +11,24 @@ class C14 {
       .toArray
   }
 
-  def getBestKeyFromLines(lines: Array[String]): Unit = {
-    val c = new C13
+  def getBestKeyFromLines(lines: Array[String], c: C13, bestLine, bestScore): Unit = {
     // Use breakSIngleByeXORCipher on each line.
     // Use this to get the lowest plain text score
+    lines match {
+      case (h,t) => {
+        val lineScore = c.findBestKey(h, 0, Double.MaxValue, 0)
+        if (lineScore < bestScore){
+          getBestKeyFromLines(t, c, h, lineScore)
+        }
+        else {
+          getBestKeyFromLines(t, c, bestLine, bestScore)
+        }
+      }
+      case (_) => {
+        // return the keys here?
+        bestLine
+      }
+    }
   }
 
   def detectSingleCharacterXOR(filename: String): String = {
