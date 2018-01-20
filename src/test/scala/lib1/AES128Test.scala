215b2cddccd696f54fb59fe3809f1cb95de6738d
malancas <malancaster0@gmail.com>
2018-01-20 12:53:57 -0500

    Added lookup s box array and rcon array. Started adding functions for AES steps

--- src/test/scala/lib1/AES128Test.scala
+++ src/test/scala/lib1/AES128Test.scala
@@ -8,12 +8,16 @@ class AES128Test extends FunSpec with Matchers {
   describe("AES128") {
     val aes128 = new AES128Impl
 
+    /*
     it("sub1 should convert ints to their sbox result") {
-      aes128.substitute1(0) shouldBe 0x63 // 124
-    }    
-
+      //aes128.substitute1(0) shouldBe 0x63 // 130
+      //aes128.substitute1(17) shouldBe 0x82 // 124
+      aes128.substitute1(0x9a) shouldBe 0xb8 // 184     
+    }   
+    */
+    
     it("sub2 should convert ints to their sbox result") {
-      aes128.substitute2(0) shouldBe 0x63 // 124
+      aes128.substitute3(0x9a) shouldBe 0xb8
     }
   }
 }
