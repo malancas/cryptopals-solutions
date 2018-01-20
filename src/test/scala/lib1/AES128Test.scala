8a989ca7a039bf5951131a2dbda94071e34b4b9a
malancas <malancaster0@gmail.com>
2018-01-20 12:54:27 -0500

    Remove unneeded tests

--- src/test/scala/lib1/AES128Test.scala
+++ src/test/scala/lib1/AES128Test.scala
@@ -5,20 +5,6 @@ import org.scalatest._
 import lib1.AES128
 
 class AES128Test extends FunSpec with Matchers {
-  describe("AES128") {
-    val aes128 = new AES128Impl
-
-    /*
-    it("sub1 should convert ints to their sbox result") {
-      //aes128.substitute1(0) shouldBe 0x63 // 130
-      //aes128.substitute1(17) shouldBe 0x82 // 124
-      aes128.substitute1(0x9a) shouldBe 0xb8 // 184     
-    }   
-    */
-    
-    it("sub2 should convert ints to their sbox result") {
-      aes128.substitute3(0x9a) shouldBe 0xb8
-    }
-  }
+  describe("AES128") {}
 }
 
