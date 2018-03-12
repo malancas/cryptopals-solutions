4d0a58a2138f6ecd3e34613bd7065fff16e72539
malancas <malancaster0@gmail.com>
2018-03-11 22:08:09 -0400

    Added shiftRows test

--- src/test/scala/lib1/AES128Test.scala
+++ src/test/scala/lib1/AES128Test.scala
@@ -33,4 +33,12 @@ class AES128Test extends FunSpec with Matchers with BeforeAndAfterEach with Mock
       aes128Impl.addRoundKey(column, roundKey) shouldBe column
     }
   }
+
+  describe("shiftRows") {
+    it ("should return shifted 0-16 array") {
+      val row = Array.range(0,16)
+      val shiftedRow = Array(0,1,2,3,5,6,7,4,10,11,8,9,15,12,13,14)
+      aes128Impl.shiftRows(row) shouldBe shiftedRow
+    }
+  }
 }
