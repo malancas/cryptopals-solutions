ba1582a1a39365574e12f89b917b095d11410733
malancas <malancaster0@gmail.com>
2018-03-21 21:12:07 -0400

    Separated Rijndael key scheduler functions into a separate class

--- src/main/scala/lib1/AES128.scala
+++ src/main/scala/lib1/AES128.scala
@@ -2,7 +2,7 @@ package lib1
 
 trait AES128 {}
 
-class AES128Impl(key: String, ciphertext: String) extends AES128 {
+class AES128Impl(key: String, ciphertext: String, rijndaelKeyScheduler: RijndaelKeyScheduler) extends AES128 {
    // Initialized s box
   val rijndaelSBox = Array(
     0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0x76,
@@ -22,8 +22,6 @@ class AES128Impl(key: String, ciphertext: String) extends AES128 {
     0xE1, 0xF8, 0x98, 0x11, 0x69, 0xD9, 0x8E, 0x94, 0x9B, 0x1E, 0x87, 0xE9, 0xCE, 0x55, 0x28, 0xDF,
     0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68, 0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16
   )
-  // Initialized rcon
-  val rcon = Array(0x8d, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36)
 
   def doAlgorithm: String = {
     val keyArray = key.split("").map(_.toInt)
@@ -36,68 +34,35 @@ class AES128Impl(key: String, ciphertext: String) extends AES128 {
     val newCipherState = addRoundKey(cipherState, roundKey)
 
     // Do 9 main rounds
-    doMainRounds(0, cipherState)
+    val newCipherState = doMainRounds(0, cipherState, roundKey)
 
-    "filler"
+    // Do final round
+    doFinalRound(newCipherState)
   }
 
   def doMainRounds(i: Int, cipherState: Array[Int]): Array[Int] = {
-    // Substitute the ciphertext values for their s-box equivalent
-    val subbedCipher = subBytes(cipherState)
+    if (i < 9) {
+      // Substitute the ciphertext values for their s-box equivalent
+      val subbedCipher = subBytes(cipherState)
+      // Shift each row of the ciphertext
+      val shiftedCipher = shiftRows(subbedCipher)
+      // Mix columns
+      val mixedCipher = mixColumns(shiftedCipher)
+      // Do addRoundKey
+      val newCipherState = addRoundKey(mixedCipher, roundKey)
+      
+      doMainRounds(i+1, newCipherState)
+    } else cipherState
+  }
 
-    // Shift each row of the ciphertext
+  def doFinalRound(cipher: Array[Int]): Array[Int] = {
+    val subbedCipher = subBytes(newCipherState)
     val shiftedCipher = shiftRows(subbedCipher)
-
-    // Mix columns
-    val mixedCipher = mixColumns(shiftedCipher)
-    Array(1)
+    val addedCipher = addRoundKey(shiftedCipher)
   }
 
   def createRoundKey(keyArray: Array[Int]): Array[Int] = {
     rijndaelKeyScheduler(1, keyArray)
-  }
-
-  def keySchedulerCore(input: Array[Int], i: Int): Array[Int] = {
-    // Rotate the input word eight bits to the left
-    val rotated = input.slice(1, 16) ++ Array(input(0))
-
-    // Apply the S-Box on all four elements of the word
-    val sBoxWord = rotated.map(rijndaelSBox(_))
-
-    // XOR the first byte of the word with the rcon output when using i
-    val newFirstByte = rcon(i) ^ sBoxWord(0)
-
-    Array(newFirstByte) ++ sBoxWord.slice(1, sBoxWord.length)
-  }
-
-  def produce12BytesOfExpandedKey(i: Int, currKey: Array[Int]): Array[Int] = {
-    if (i < 3){
-      // Append the last four bytes of the current extended key to temp
-      val temp = currKey.takeRight(4)
-      // XOR t with the four-byte block n bytes before the new expanded key.
-      // This becomes the next 4 bytes in the expanded key
-      val beginIt = currKey.length - 1 - 16 - 3
-      val endIt = currKey.length - 1 - 16
-      val nextFourBytes = temp.zip(currKey.slice(beginIt, endIt)).map{ case (x, y) => x ^ y }
-
-      produce12BytesOfExpandedKey(i+1, currKey ++ nextFourBytes)
-    }
-    currKey
-  }
-
-  def rijndaelKeyScheduler(rconIteration: Int, currKey: Array[Int]): Array[Int] = {
-    if (currKey.length < 176) {
-      // Create the next four bytes of the expanded key
-      val temp = currKey.takeRight(4)
-      val currExpandedKey = keySchedulerCore(temp, rconIteration)
-
-      // Do the following three times to create the next twelve bytes of expanded key
-      val newBytes = produce12BytesOfExpandedKey(0, currExpandedKey)
-      rijndaelKeyScheduler(rconIteration+1, currKey ++ newBytes)
-    }
-    else {
-      currKey
-    }
   }
 
   // Each element of the cipher state is XORed with the round key's equivalent element
---
+++ src/main/scala/lib1/RijndaelKeyScheduler.scala
@@ -0,0 +1,49 @@
+package lib1
+
+class RijndaelKeyScheduler {
+  // Initialized rcon
+  val rcon = Array(0x8d, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36)
+  
+  def rijndaelKeyScheduler(rconIteration: Int, currKey: Array[Int]): Array[Int] = {
+    if (currKey.length < 176) {
+      // Create the next four bytes of the expanded key
+      val temp = currKey.takeRight(4)
+      val currExpandedKey = keySchedulerCore(temp, rconIteration)
+
+      // Do the following three times to create the next twelve bytes of expanded key
+      val newBytes = produce12BytesOfExpandedKey(0, currExpandedKey)
+      rijndaelKeyScheduler(rconIteration+1, currKey ++ newBytes)
+    }
+    else {
+      currKey
+    }
+  }
+
+  def keySchedulerCore(i: Int, input: Array[Int]): Array[Int] = {
+    // Rotate the input word eight bits to the left
+    val rotated = input.slice(1, 16) ++ Array(input(0))
+
+    // Apply the S-Box on all four elements of the word
+    val sBoxWord = rotated.map(rijndaelSBox(_))
+
+    // XOR the first byte of the word with the rcon output when using i
+    val newFirstByte = rcon(i) ^ sBoxWord(0)
+
+    Array(newFirstByte) ++ sBoxWord.slice(1, sBoxWord.length)
+  }
+
+  def produce12BytesOfExpandedKey(i: Int, currKey: Array[Int]): Array[Int] = {
+    if (i < 3){
+      // Append the last four bytes of the current extended key to temp
+      val temp = currKey.takeRight(4)
+      // XOR t with the four-byte block n bytes before the new expanded key.
+      // This becomes the next 4 bytes in the expanded key
+      val beginIt = currKey.length - 1 - 16 - 3
+      val endIt = currKey.length - 1 - 16
+      val nextFourBytes = temp.zip(currKey.slice(beginIt, endIt)).map{ case (x, y) => x ^ y }
+
+      produce12BytesOfExpandedKey(i+1, currKey ++ nextFourBytes)
+    }
+    currKey
+  }
+}
